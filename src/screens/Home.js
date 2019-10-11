83d8065698dfb978abaa1e700d68c06fdee100b8
anton.miroshnichenko <anton.miroshnichenko@ix.co>
2019-10-11 11:40:19 +0300

    React Base

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,36 +1,39 @@
-import React, {useState, useEffect} from 'react';
-import {View, Animated, Text} from 'react-native';
+import React from 'react';
+import {Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text} from 'native-base';
 
 const HomeScreen = ({navigation: {navigate}}) => {
-    const [fadeAnimation] = useState(new Animated.Value(0));
-    const [position] = useState(new Animated.Value({x:100, y:100}));
-
-    React.useEffect(() => {
-        Animated.parallel([
-            Animated.timing(
-                fadeAnimation,
-                {
-                    toValue: 1,
-                    duration: 10000
-                }
-            ),
-            Animated.spring(
-                position,
-                {
-                    toValue: {x: 0, y: 0},
-                    duration: 5000
-                }
-            )
-        ]).start();
-    }, []);
-
     return (
-        <View>
-            <Animated.View style={{opacity: fadeAnimation, position: 'absolute', top: position.y, left: position.x}}>
-                <Text>Home Screen</Text>
-            </Animated.View>
-        </View>
+        <Container>
+            <Content>
+                <Text>
+                    This is Content Section
+                </Text>
+            </Content>
+            <Footer>
+                <FooterTab>
+                    <Button full>
+                        <Text>Footer</Text>
+                    </Button>
+                </FooterTab>
+            </Footer>
+        </Container>
     );
 };
+
+HomeScreen.navigatorOptions = {
+    header: () => (
+        <Header>
+            <Left>
+                <Button transparent>
+                    <Icon name='menu'/>
+                </Button>
+            </Left>
+            <Body>
+                <Title>Header</Title>
+            </Body>
+            <Right/>
+        </Header>
+    )
+}
 
 export default HomeScreen;
