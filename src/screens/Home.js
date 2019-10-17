89002506e66f41b7c8c3b16b995b811066e8b0eb
anton.miroshnichenko <anton.miroshnichenko@ix.co>
2019-10-17 13:41:47 +0300

    Firebase

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import {Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text} from 'native-base';
 import TodoList from '@components/TodoList';
+import firebase from 'react-native-firebase';
 
 const HomeScreen = ({navigation: {navigate}}) => {
+    firebase.auth()
+        .signInAnonymously()
+        .then(credential => {
+            if (credential) {
+                console.log('default app user ->', credential.user.toJSON());
+            }
+        });
     return (
         <Container>
             <Content>
