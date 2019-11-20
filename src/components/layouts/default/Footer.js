51eb9a0080557dbe05d8d0b9b42c7d767cef0760
anton.miroshnichenko <anton.miroshnichenko@ix.co>
2019-11-20 12:02:52 +0300

    Footer Text

--- src/components/layouts/default/Footer.js
+++ src/components/layouts/default/Footer.js
@@ -7,19 +7,15 @@ const AppFooter = ({navigation}) => {
             <FooterTab>
                 <Button vertical active={navigation.state.routeName === 'Home'}>
                     <Icon type="Feather" name="home" />
-                    <Text>Home</Text>
                 </Button>
                 <Button vertical active={navigation.state.routeName === 'News'}>
                     <Icon type="Feather" name="book-open" />
-                    <Text>News</Text>
                 </Button>
                 <Button vertical active={navigation.state.routeName === 'Inbox'}>
                     <Icon type="Feather" name="inbox" />
-                    <Text>Inbox</Text>
                 </Button>
                 <Button vertical active={navigation.state.routeName === 'Menu'}>
                     <Icon type="Feather" name="menu" />
-                    <Text>Menu</Text>
                 </Button>
             </FooterTab>
         </Footer>
